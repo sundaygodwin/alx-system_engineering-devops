d8d88d4061765685a06d99120203d80f1a3d1ae8
sundaygodwin <godwinsunday194@gmail.com>
2023-11-28 13:30:38 -0800

    correction advance

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,19 +1,11 @@
 #!/usr/bin/env ruby
 # Sample data (you'll need to replace this with actual data retrieval logic)
 # Assume messages are stored in a list of dictionaries or any appropriate data structure
-messages_data = [
-    {"sender": "+123456789", "receiver": "+987654321", "flags": "URGENT"},
-    {"sender": "+987654321", "receiver": "+123456789", "flags": "NORMAL"},
-    # Add more messages here
-]
-
 # Function to process and extract statistics
-def extract_message_statistics(data):
-    for message in data:
-        sender = message.get("sender", "Unknown")
-        receiver = message.get("receiver", "Unknown")
-        flags = message.get("flags", "Unknown")
-        print(f"{sender},{receiver},{flags}")
 
-# Run the function with the provided data
-extract_message_statistics(messages_data)
+SENDER = ARGV[0].scan(/from:\+*\w*/).join[5..-1]
+RECEIVER = ARGV[0].scan(/to:\+*\w*/).join[3..-1]
+FLAGS = ARGV[0].scan(/flags:(.*?)\]/).join
+
+msg = SENDER + "," + RECEIVER + "," + FLAGS
+puts msg
